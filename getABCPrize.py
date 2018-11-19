8fe4e5773407096e6ba44e637470d38e5f86d6b0
Fongim <lemonjush@163.com>
2018-11-20 00:35:07 +0800

    181120

--- getABCPrize.py
+++ getABCPrize.py
@@ -52,11 +52,11 @@ def lottery(postitionindex, uniqid, years, months, destination, cookies):
 
 
 def run():
-    with open('./深圳联通O粉卡号后半.txt', 'r') as fr:
+    with open('./深圳联通米粉卡号1119.txt', 'r') as fr:
         mobile_numbers = fr.readlines()
 
     for mobile_number in mobile_numbers:
-        with open('./深圳联通O粉卡输出.txt', 'a') as all_output, open('./bingo.txt', 'a') as bingo_output:
+        with open('./深圳联通米粉卡号输出1119.txt', 'a') as all_output, open('./bingo.txt', 'a') as bingo_output:
             mobile_number = mobile_number.strip()
 
             for index in range(4):
